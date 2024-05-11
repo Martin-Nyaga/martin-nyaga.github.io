5ec9a0b96c630d34aee03ca872fbd43211e80729
Martin Nyaga <martin@martinnyaga.com>
2024-05-11 01:29:10 +0100

    Improve mobile layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), alpinejs()],
   site: "https://martin-nyaga.github.io"
 });
