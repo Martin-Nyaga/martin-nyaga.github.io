ba3e61591e76ea6a3cb091a1cf96a9166afc8446
Martin Nyaga <martin@martinnyaga.com>
2026-06-03 01:44:14 +0100

    First pass redesign

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
 	theme: {
 		extend: {
       fontFamily: {
-        'sans': ['Recursive', 'Monospace']
+        'sans': ['IBM Plex Sans', 'system-ui', 'sans-serif']
       }
     },
 	},
